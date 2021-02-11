18e98f934bc37b632a51e9af8127f762f38cb4d7
nanhangl <58681238+nanhangl@users.noreply.github.com>
2021-02-11 21:42:08 +0800

    Update main.js

--- SITConnect/js/main.js
+++ SITConnect/js/main.js
@@ -58,7 +58,7 @@ if (location.pathname == "/Register") {
     })
 
     function checkEmail() {
-        if (/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(document.querySelector("#MainContent_tbEmail").value)) {
+        if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(document.querySelector("#MainContent_tbEmail").value)) {
             document.querySelector("#MainContent_tbEmail").style.border = "1px solid #42dc00";
             regEmailOk = true;
         } else {
